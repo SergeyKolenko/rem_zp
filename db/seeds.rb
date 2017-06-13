a49f5fde210e0e0926242969646474403c90cfa1
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2017-06-13 16:49:46 +0300

    Added Region model spec

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,9 @@
-regions = ['Autonomous Republic of Crimea', 'Cherkasy', 'Chernihiv', 'Chernivtsi',
-           'Dnipropetrovsk', 'Donetsk', 'Ivano-Frankivsk', 'Kharkiv', 'Kherson',
-           'Khmelnytskyi', 'Kiev', 'Kirovohrad', 'Luhansk', 'Lviv', 'Mykolaiv',
-           'Odessa', 'Poltava', 'Rivne', 'Sumy', 'Ternopil', 'Vinnytsia',
-           'Volyn', 'Zakarpattia', 'Zaporizhia', 'Zhytomyr']
+regions = ['АР Крым', 'Винницкая область', 'Волынская область', 'Днепропетровская область',
+           'Донецкая область', 'Житомирская область', 'Закарпатская область',
+           'Запорожская область', 'Ивано-Франковская область', 'Киевская область',
+           'Кировоградская область', 'Луганская область', 'Львовская область',
+           'Николаевская область', 'Одесская область', 'Полтавская область',
+           'Ровненская область', 'Сумская область', 'Тернопольская область',
+           'Харьковская область', 'Херсонская область', 'Хмельницкая область',
+           'Черкасская область', 'Черниговская область', 'Черновицкая область']
 regions.each { |region| Region.find_or_create_by(name: region) }
-
-
-
-
