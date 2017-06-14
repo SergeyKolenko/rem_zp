baf6bc4f5e386f0b0be1609d75ea2e74cde16794
krasno86 <krasno86@mail.ru>
2017-06-14 17:58:48 +0300

    fix tests models association and column types

--- spec/models/option_value_spec.rb
+++ spec/models/option_value_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe OptionValue, type: :model do
     it { should belong_to(:proposal) }
     it { should belong_to(:option) }
   end
+
+  context 'OptionValue db column' do
+    it { should have_db_column(:value).of_type(:string) }
+    it { should have_db_column(:proposal_id).of_type(:integer) }
+    it { should have_db_column(:option_id).of_type(:integer) }
+  end
 end
