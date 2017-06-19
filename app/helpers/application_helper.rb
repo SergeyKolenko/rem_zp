dbd3282a9e1cc5993d549fb0bf66e57b7d9d91b6
SergeyKolenko <venty80@gmail.com>
2017-06-19 08:45:45 +0300

    intermediate commit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+
+  def meta_title
+    @meta_title || 'Недвижимость в Украине'
+  end
+
+  def meta_keywords
+    @meta_keywords || 'Покупка, продажа, аренда, жилье, склад, квартира, комната, дача, гараж'
+  end
+
+  def meta_description
+    @meta_description || 'Покупка, продажа, аренда жилья в Украине'
+  end
+
+  def all_categories
+    Rails.cache.fetch("all_categories_#{I18n.locale}", expired_at: 24.hours) do
+      Categories.all
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,11 @@ class User < ApplicationRecord
   has_many :proposals, foreign_key: :author_id
   has_many :agencies, foreign_key: 'director_id'
 
+  delegate :name, to: :role, prefix: true
+
   validates :first_name, :last_name, :role, presence: true
-  validates :agency, presence: true, if: Proc.new { |u| %w(agent agency_director).include? u.role.name }
-  validates :agency, absence: true, unless: Proc.new { |u| %w(agent agency_director).include? u.role.name }
+  validates :agency, presence: true, if: Proc.new { |u| %w(agent agency_director).include? u.role_name }
+  validates :agency, absence: true, unless: Proc.new { |u| %w(agent agency_director).include? u.role_name }
   validates :phone, format: { with: /[0]\d[(39|50|63|66|67|68|91|92|93|94|95|96|97|98|99)]-\d{3}-\d{2}-\d{2}/,
                               message: 'Неверный формат номера' }
 end
