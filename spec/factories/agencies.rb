4f10b8c846091f0a9ddae7e550b39aef6fc24a57
krasno86 <krasno86@mail.ru>
2017-06-20 18:09:08 +0300

    need fix agency_spec,option_value_spec,proposal_spec

--- spec/factories/agencies.rb
+++ spec/factories/agencies.rb
@@ -3,13 +3,21 @@ FactoryGirl.define do
     name Faker::StarWars.planet
     postal_address Faker::Address.secondary_address
     phisical_address Faker::Address.street_address
-    approved true
-    # region
-    # director
-    # user
-    # city
-    # district
-    phone Faker::Number.number(10)
+
+    trait :approved_false do
+      approved false
+    end
+
+    trait :approved_true do
+      approved true
+    end
+
+    trait :valid_phone do
+      phone '066-965-26-17'
+    end
+    trait :invalid_phone do
+      phone '8669652617'
+    end
   end
 end
 
