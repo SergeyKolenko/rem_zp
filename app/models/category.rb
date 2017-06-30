b23630a9225e262a3b166e99315dbf9560365f52
krasno86 <krasno86@mail.ru>
2017-06-30 14:25:58 +0300

    Fixed modal window

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,6 @@ class Category < ApplicationRecord
 
   validates :name, :weight, presence: true
   validates :weight, numericality: { greater_than: 0 }
+
+  default_scope { order(weight: :desc) }
 end
