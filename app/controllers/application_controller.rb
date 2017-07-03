165faaf4199733ff94ee52260eef6d1f63a14099
SergeyKolenko <venty80@gmail.com>
2017-07-03 08:55:41 +0300

    code style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,14 +7,12 @@ class ApplicationController < ActionController::Base
   end
 
   protected
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :role_id, :phone,
-                                                       owned_agency_attributes: [:director_id, :name, :logo, :phone,
-                                                                                 :postal_address, :phisical_address,
-                                                                                 :description, :approved,
-                                                                                 :city_ids, :region_ids, :district_ids]
-    ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [
+        :first_name, :last_name, :role_id, :phone, owned_agency_attributes: [
+        :director_id, :name, :logo, :phone, :postal_address, :phisical_address, :description, :approved, :city_ids,
+        :region_ids, :district_ids
+    ]])
   end
 
   def current_ability
