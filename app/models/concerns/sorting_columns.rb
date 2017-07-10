4b5867faeb841a3a470e67577e24d22d126d164a
krasno86 <krasno86@mail.ru>
2017-07-10 13:54:57 +0300

    try fix SQL injection

--- app/models/concerns/sorting_columns.rb
+++ app/models/concerns/sorting_columns.rb
@@ -2,10 +2,10 @@ module SortingColumns
   extend ActiveSupport::Concern
 
   included do
-    scope :sorting, -> (field_name = 'created_at', order_type = 'asc') do
+    scope :sorting, -> (field_name = 'id', order_type = 'asc') do
       column = self.column_names.include?(field_name) ? field_name : 'created_at'
       sort = %w(asc desc).include?(order_type) ? order_type : 'asc'
-      order %Q(#{field_name} #{order_type})
+      order field_name => order_type
     end
   end
 end
