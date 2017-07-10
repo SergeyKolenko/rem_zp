5e082ff72a52e2b84cdd587c7322fef46429857f
Shabelnik Max <maksim.shabelnik@faceit-team.com>
2017-07-10 15:43:46 +0300

    Fixing issue with agency director creation.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,9 +10,9 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [
-        :first_name, :last_name, :role_id, :phone, owned_agency_attributes: [
-        :director_id, :name, :logo, :phone, :postal_address, :phisical_address, :description, :approved, :city_ids,
-        :region_ids, :district_ids
+        :first_name, :last_name, :role_id, :phone, :locale, :avatar_cache, owned_agency_attributes: [
+        :director_id, :name, :logo, :phone, :postal_address, :phisical_address, :description, :approved, { city_ids: [] },
+        { region_ids: [] }, { district_ids: [] }
     ]])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :phone, :locale, :avatar,
                                                               :remove_avatar, :avatar_cache])
--- app/models/agency.rb
+++ app/models/agency.rb
@@ -8,11 +8,12 @@ class Agency < ApplicationRecord
   mount_uploader :logo, AgencyLogoUploader
 
   validates :name, :postal_address, :phisical_address, presence: true
-  validates :director, inclusion: { in: :valid_users }
+  #validates :director, inclusion: { in: :valid_users }
   validates :phone, format: { with: /\A[0]\d[(39|50|63|66|67|68|91|92|93|94|95|96|97|98|99)]-\d{3}-\d{2}-\d{2}\z/,
                               message: I18n.t('shared.wrong_phone') }
 
   private
+
   def valid_users
     Role.agency_director.try(:users) || []
   end
