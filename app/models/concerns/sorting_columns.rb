d2274832f6912b0f126956e5550157a4e6dd3943
krasno86 <krasno86@mail.ru>
2017-07-10 14:53:55 +0300

    try fix raw

--- app/models/concerns/sorting_columns.rb
+++ app/models/concerns/sorting_columns.rb
@@ -2,10 +2,10 @@ module SortingColumns
   extend ActiveSupport::Concern
 
   included do
-    scope :sorting, -> (field_name = 'id', order_type = 'asc') do
-      column = self.column_names.include?(field_name) ? field_name : 'created_at'
+    scope :sorting, -> (field_name = 'created_at', order_type = 'asc') do
+      column = self.column_names.include?(field_name) ? field_name : 'id'
       sort = %w(asc desc).include?(order_type) ? order_type : 'asc'
-      order field_name => order_type
+      order column => sort
     end
   end
 end
