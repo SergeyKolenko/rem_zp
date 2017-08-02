13f5e519128e816cd852d39ea4a328eb1c649637
krasno86 <krasno86@mail.ru>
2017-08-02 15:51:41 +0300

    Fixed log_in + sign_out tests

--- spec/features/log_in_spec.rb
+++ spec/features/log_in_spec.rb
@@ -6,7 +6,7 @@ describe "the sign in process", type: :feature do
   let!(:role_realtor) { FactoryGirl.create(:realtor) }
   let!(:user_valid) { FactoryGirl.create(:user, role: role_simple_user, confirmed_at: Time.now) }
 
-  it 'log_in and sign out simple user' , js: true do
+  it 'log_in and sign out simple user' do
     visit '/user/sign_in'
     within 'form' do
       fill_in 'user_email', with: user_valid.email
@@ -16,7 +16,7 @@ describe "the sign in process", type: :feature do
     expect(page).to have_content 'Вошли успешно.'
   end
 
-  it 'log in simple user with wrong email' , js: true do
+  it 'log in simple user with wrong email' do
     visit '/user/sign_in'
     p user_valid
     within 'form' do
--- spec/features/sign_out_spec.rb
+++ spec/features/sign_out_spec.rb
@@ -6,7 +6,7 @@ describe "the sign in process", type: :feature do
   let!(:role_realtor) { FactoryGirl.create(:realtor) }
   let!(:user_valid) { FactoryGirl.create(:user, role: role_simple_user, confirmed_at: Time.now) }
 
-  it 'sign out simple user' , js: true do
+  it 'sign out simple user' do
     visit '/user/sign_in'
     within 'form' do
       fill_in 'user_email', with: user_valid.email
